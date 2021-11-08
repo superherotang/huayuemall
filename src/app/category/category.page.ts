e000415fe1702ccb3adb51727714a903ad8b10c6
alectang <445595661@qq.com>
2021-11-08 23:37:04 +0800

    master

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
+  menuList = [
+    {
+      menuName: "测试菜单1",
+      nameList:[
+        {
+          categoryName:"测试菜单1分类"
+        }
+      ]
+    }, {
+      menuName: "测试菜单2"
+    }, {
+      menuName: "测试菜单3"
+    }, {
+      menuName: "测试菜单4"
+    }, {
+      menuName: "测试菜单5"
+    }, {
+      menuName: "测试菜单6"
+    }, {
+      menuName: "测试菜单7"
+    }, {
+      menuName: "测试菜单8"
+    }, {
+      menuName: "测试菜单9"
+    }, {
+      menuName: "测试菜单10"
+    }, {
+      menuName: "测试菜单11"
+    }, {
+      menuName: "测试菜单12"
+    }, {
+      menuName: "测试菜单13"
+    },{
+      menuName: "测试菜单14"
+    },{
+      menuName: "测试菜单15"
+    },
+  ]
 
   constructor() { }
 
